3df7042eea145409893359e2db42b074396b8978
Lynette Aglen <lynette.aglen@gmail.com>
2020-04-09 19:47:06 +0200

    Added about and made changes to home

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import App from "./App";
-
+import Home from './pages/Home';
+import About from './pages/About';
 
 ReactDOM.render(
   <Router>
     <App>
       <Switch>
-
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
       </Switch>
     </App>
   </Router>,
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,15 +2,22 @@ import React, { useState } from 'react';
 
 
 export default function Home() {
-    const [type, setType] = useState('');
+    /* const [type, setType] = useState('');
 
 
     const typeText = () => {
 
-    }
+    } */
 
     return (
         <>
+            <div className="home__container">
+                <div className="home__wrapper">
+                    <h1 className="home__text">Welcome.</h1>
+                </div>
+
+                <h2>&lt;h1&gt;home&lt;/h1&gt;</h2>
+            </div>
         </>
     )
 }
