e32f7c0dd398408d88c60db8e4cfb5ac1519837a
Lynette Aglen <lynette.aglen@gmail.com>
2020-04-14 22:36:14 +0200

    updated app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from "./components/navigation";
+import Footer from "./components/footer";
 import "bootstrap3/dist/css/bootstrap.min.css";
 import "./scss/styles.scss";
 
@@ -10,6 +11,7 @@ export default function App({ children }) {
     <div className="App">
       <Navigation />
       <div>{children}</div>
+      <Footer />
     </div>
   )
 }
