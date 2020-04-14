3e39f865ababd9d617b318d5d3115a197d3dc9d0
Lynette Aglen <lynette.aglen@gmail.com>
2020-04-14 23:39:49 +0200

    Added type effect

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,21 +2,35 @@ import React, { useState } from 'react';
 
 
 export default function Home() {
-    /* const [type, setType] = useState('');
+    const [type, setType] = useState('');
 
 
-    const typeText = () => {
 
-    } */
 
     return (
         <>
             <div className="home__container">
                 <div className="home__wrapper">
-                    <h1 className="home__text">welcome.</h1>
+                    <div className="css-typing p">
+                        <p>let greeting = 'Welcome.';</p>
+                        <p>console.log(greeting);</p>
+                    </div>
                 </div>
+            </div>
+            {/*  <h2>&lt;h1&gt;home&lt;/h1&gt;</h2> */}
 
-                <h2>&lt;h1&gt;home&lt;/h1&gt;</h2>
+            <div className="project-container">
+                <h2 className="project-container__head">My school projects</h2>
+
+                <h2 className="project-container__sub">Semester Project 2020</h2>
+                <a>link to project</a>
+
+                <h2 className="project-container__sub">Exam 2019</h2>
+                <a>link to project</a>
+
+                <h2 className="project-container__sub">Semester Project 2019</h2>
+                <a>link to project</a>
+
             </div>
         </>
     )
