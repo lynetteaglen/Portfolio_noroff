814c0612e5173ea42dfa7a52cd9c1c5d5e86e372
Lynette Aglen <lynette.aglen@gmail.com>
2020-04-23 23:17:10 +0200

    updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,8 +12,9 @@ export default function About() {
                 </p>
                 <p className="about__paragraph-two">
                     A few years back I was introduced to the world of technology by my best friend and
-                    she opened my eyes for all the exciting things tech can provide the world with. I took a leap of faith and changed my career path after being amazed by the things I was told and feeling a growth of passion. I wanted to explore my creativeness and
-                    push my self into this interesting (but also super confusing) world of programming and design.</p>
+                    she opened my eyes for all the exciting things tech can provide the world with. I took a leap of faith and changed my career path after
+                    being amazed by the things I was told and feeling a growth of passion.
+                    I wanted to explore my creativity and push my self into this interesting (but also super confusing) world of programming and design.</p>
                 <p className="about__paragraph-three">
                     Now I am about to finish the 2 year programme at Noroff to become a Frontend Developer. I am more excited and hungry for technology than ever, and I am open to
                     continue my growth and skillset.
