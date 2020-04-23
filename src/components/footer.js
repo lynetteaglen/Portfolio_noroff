3a9a43f9dcfa25021efd01e2e9c282abb69cd028
Lynette Aglen <lynette.aglen@gmail.com>
2020-04-23 23:16:10 +0200

    changed styling in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,14 @@ import React from 'react';
 const Footer = () => {
     return (
         <>
-            <footer>Need to insert icons</footer>
+            <footer>
+                <div className="footer__icons">
+                    <img className="footer__github" src="https://img.icons8.com/ios-filled/50/000000/github.png" />
+                    <img className="footer__linkedin" src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled.png" />
+                </div>
+                <p className="footer__text">© 2020 Lynette Aglen. All rights reserved</p>
+
+            </footer>
         </>
     )
 }
