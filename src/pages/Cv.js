229e76894a1b2b1a1f2ef5b544266ab78989820f
Lynette Aglen <lynette.aglen@gmail.com>
2020-04-25 19:41:40 +0200

    Fixed layout on cv

--- src/pages/Cv.js
+++ src/pages/Cv.js
@@ -10,98 +10,98 @@ export default function Cv() {
                 <h2 className="cv__sub">skills</h2>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>JavaScript</p></li>
-                    <li className="cv__list"><p>React.js</p></li>
-                    <li className="cv__list"><p>HTML/CSS + framworks</p></li>
+                    <li className="cv__list-1"><p>JavaScript</p></li>
+                    <li className="cv__list-2"><p>React.js</p></li>
+                    <li className="cv__list-3"><p>HTML/CSS + framworks</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>Visual Studio Code</p></li>
-                    <li className="cv__list"><p>Github</p></li>
-                    <li className="cv__list"><p>Adobe Creative Suite</p></li>
+                    <li className="cv__list-1"><p>Visual Studio Code</p></li>
+                    <li className="cv__list-2"><p>Github</p></li>
+                    <li className="cv__list-3"><p>Adobe Creative Suite</p></li>
                 </ul>
                 <hr></hr>
 
                 <h2 className="cv__sub">experience</h2>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2013</p></li>
-                    <li className="cv__list"><p>Gardnos Meteorite Park</p><p>Tourist-guide/Waitress</p></li>
-                    <li className="cv__list"><p>Nesbyen</p></li>
+                    <li className="cv__list-1"><p>2013</p></li>
+                    <li className="cv__list-2"><p>Gardnos Meteorite Park</p><p>Tourist-guide/Waitress</p></li>
+                    <li className="cv__list-3"><p>Nesbyen</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2015</p></li>
-                    <li className="cv__list"><p>På Hjørnet</p><p>Barista/Waitress</p></li>
-                    <li className="cv__list"><p>Nesbyen</p></li>
+                    <li className="cv__list-1"><p>2015</p></li>
+                    <li className="cv__list-2"><p>På Hjørnet</p><p>Barista/Waitress</p></li>
+                    <li className="cv__list-3"><p>Nesbyen</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2016</p></li>
-                    <li className="cv__list"><p>Skin Tonic</p><p>Shop Assistant</p></li>
-                    <li className="cv__list"><p>Gol</p></li>
+                    <li className="cv__list-1"><p>2016</p></li>
+                    <li className="cv__list-2"><p>Skin Tonic</p><p>Shop Assistant</p></li>
+                    <li className="cv__list-3"><p>Gol</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2016-2017</p></li>
-                    <li className="cv__list"><p>Storefjell Hotel Resort</p><p>waitress</p></li>
-                    <li className="cv__list"><p>Gol</p></li>
+                    <li className="cv__list-1"><p>2016-2017</p></li>
+                    <li className="cv__list-2"><p>Storefjell Hotel Resort</p><p>waitress</p></li>
+                    <li className="cv__list-3"><p>Gol</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2018</p></li>
-                    <li className="cv__list"><p>Elverhøy Helsetun</p><p>Summer Substitute</p></li>
-                    <li className="cv__list"><p>Nesbyen</p></li>
+                    <li className="cv__list-1"><p>2018</p></li>
+                    <li className="cv__list-2"><p>Elverhøy Helsetun</p><p>Summer Substitute</p></li>
+                    <li className="cv__list-3"><p>Nesbyen</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2019-2020</p></li>
-                    <li className="cv__list"><p>Noroff</p><p>Student Ambassador</p></li>
-                    <li className="cv__list"><p>Oslo</p></li>
+                    <li className="cv__list-1"><p>2019-2020</p></li>
+                    <li className="cv__list-2"><p>Noroff</p><p>Student Ambassador</p></li>
+                    <li className="cv__list-3"><p>Oslo</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2019-Present</p></li>
-                    <li className="cv__list"><p>Smarthotel</p><p>Receptionist</p></li>
-                    <li className="cv__list"><p>Oslo</p></li>
+                    <li className="cv__list-1"><p>2019-Present</p></li>
+                    <li className="cv__list-2"><p>Smarthotel</p><p>Receptionist</p></li>
+                    <li className="cv__list-3"><p>Oslo</p></li>
                 </ul>
                 <hr></hr>
 
                 <h2 className="cv__sub">education</h2>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2013-2016</p></li>
-                    <li className="cv__list"><p>Gol Vidaregåande</p></li>
-                    <li className="cv__list"><p>Gol</p></li>
+                    <li className="cv__list-1"><p>2013-2016</p></li>
+                    <li className="cv__list-2"><p>Gol Vidaregåande</p></li>
+                    <li className="cv__list-3"><p>Gol</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2014-2015</p></li>
-                    <li className="cv__list"><p>Asotin High School</p><p>Exchange Student with EF</p></li>
-                    <li className="cv__list"><p>Washington State, US</p></li>
+                    <li className="cv__list-1"><p>2014-2015</p></li>
+                    <li className="cv__list-2"><p>Asotin High School</p><p>Exchange Student with EF</p></li>
+                    <li className="cv__list-3"><p>Washington State, US</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2017-2018</p></li>
-                    <li className="cv__list"><p>Bachelor in pre-school teaching</p><p>OsloMet</p></li>
-                    <li className="cv__list"><p>Oslo</p></li>
+                    <li className="cv__list-1"><p>2017-2018</p></li>
+                    <li className="cv__list-2"><p>Finished 1 year of a bachelors degree in pre-school teaching</p><p>OsloMet</p></li>
+                    <li className="cv__list-3"><p>Oslo</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>2018-2020</p></li>
-                    <li className="cv__list"><p>Frontend Development</p><p>Noroff - School of Technology and Digital Media</p></li>
-                    <li className="cv__list"><p>Oslo</p></li>
+                    <li className="cv__list-1"><p>2018-2020</p></li>
+                    <li className="cv__list-2"><p>Frontend Development</p><p>Noroff - School of Technology and Digital Media</p></li>
+                    <li className="cv__list-3"><p>Oslo</p></li>
                 </ul>
                 <hr></hr>
 
                 <h2 className="cv__sub">extracurricular activity</h2>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>Co-founder of Press Hallingdal, PRESS (An organisation that works for children’s rights) - Sub-organisation for Save the Children.</p></li>
+                    <li className="cv__list-extra"><p>Co-founder of Press Hallingdal, PRESS (An organisation that works for children’s rights) - Sub-organisation for Save the Children.</p></li>
                 </ul>
                 <hr></hr>
                 <ul className="cv__list-container">
-                    <li className="cv__list"><p>Board member in Futurum (Student organisation at UiO), a discussion platform for futuristic topics, technologies and innovations.</p></li>
+                    <li className="cv__list-extra"><p>Board member in Futurum (Student organisation at UiO), a discussion platform for futuristic topics, technologies and innovations.</p></li>
                 </ul>
                 <hr></hr>
 
